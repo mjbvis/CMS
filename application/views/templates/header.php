f20d56d072f1cb2daa55726cb6fcc3f8c1792e6c
Rory C-H <gneatgeek@gmail.com>
2012-01-25 23:14:23 -0800

    Converted template to MVC and set default landing page controller.

---
+++ application/views/templates/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="Robots" content="noindex, nofollow, noarchive">
+		<meta name="Description" content="Corvallis Montessori School">
+		<meta name="Author" content="Corvallis Montessori School">
+		<meta name="Rating" content="General">
+		<title><?php echo($title); ?> | CMS</title>
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+		<link rel="stylesheet" href="<?php echo base_url('assets/styles/screen.css'); ?>" type="text/css"	media="screen">
+	</head>
+	<body>
+		<div id="main">
+			<div id="header">
+				<header>
+					<div id="log">Login/Logout Stuff Here</div>
+					<img src="assets/images/cms_logo.png" alt="CMS Logo" width="585" height="67">
+					<nav id="topNav">
+						<ul>
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>
+								<a href="#">Item 3</a>
+								<ul>
+									<li>Sub1</li>
+									<li>Sub2</li>
+									<li>Sub3</li>
+								</ul>
+							</li>
+							<li>Item 4</li>
+							<li>Item 5</li>
+							<li>Item 6</li>
+							<li>Item 7</li>
+						</ul>
+					</nav>
+				</header>
+			</div>
+			<div id="sideBar">
+				<div id="alert">
+					<p>ALERT!</p>
+					<div>
+						At vero eos et accusamus et iusto odio dignissimos
+						ducimus qui blanditiis praesentium voluptatum deleniti atque
+						corrupti quos dolores et quas molestias excepturi sint occaecati
+						cupiditate non provident, similique sunt in culpa qui officia
+						deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
+						quidem rerum facilis est et expedita distinctio. Nam libero
+						tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+						quo minus id quod maxime placeat facere possimus, omnis voluptas
+						assumenda est, omnis dolor repellendus. Temporibus autem quibusdam
+						et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
+						et voluptates repudiandae sint et molestiae non recusandae. Itaque
+						earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+						voluptatibus maiores alias consequatur aut perferendis doloribus
+						asperiores repellat.
+					</div>
+				</div>
+				<ul style="list-style:none">
+					<li>Item 1</li>
+					<li>Item 2</li>
+					<li>Item 4</li>
+					<li>Item 5</li>
+					<li>Item 6</li>
+					<li>Item 7</li>
+				</ul>
+			</div>
+			<div id="content"><!-- End Header Segment -->
