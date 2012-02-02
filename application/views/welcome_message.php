77e410e8bba41ba9dbc8e7977798b35336de9857
kosw <kos.wesley@gmail.com>
2012-02-02 15:32:54 -0800

    Added libraries/Repositories.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -70,7 +70,7 @@
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>The page you are looking at blah blah blah is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
