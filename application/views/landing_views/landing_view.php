7ed0526c0bb6db936ef35496ec1a182f1004ccb2
unknown <MarkB@.(none)>
2012-02-02 21:57:11 -0800

    trying to setup the .htaccess file to allow us to omit index.php from the url

--- application/views/landing_views/landing_view.php
+++ application/views/landing_views/landing_view.php
@@ -2,13 +2,5 @@
 <h2>Header 2</h2>
 <h3>Header 3</h3>
 <p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Curabitur quis ipsum augue. Sed iaculis tempus augue, vitae
-	vulputate augue imperdiet eu. Phasellus turpis augue, sodales sit
-	amet gravida non, elementum sed eros. Cras libero massa, fringilla
-	at ultrices eget, blandit et felis. Ut ultricies gravida est ut
-	dictum. Morbi ultrices ultrices elit in lobortis. Quisque sit amet
-	mauris non dui consectetur cursus et ac quam. Nullam tempus
-	venenatis erat et tincidunt. In in diam non tortor scelerisque
-	pretium sit amet in nisl.
+	landing view
 </p>
