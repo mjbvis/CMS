50d77344d4e6956f93c316e5b6d036c7d85b25de
Justin Field <fieldju@gmail.com>
2012-02-02 20:26:01 -0800

    db connect

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'home.justin-field.com';
+$db['default']['username'] = 'cms';
+$db['default']['password'] = 'Th3.D@t@bas3.P@ssw0rd';
+$db['default']['database'] = 'cms';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
