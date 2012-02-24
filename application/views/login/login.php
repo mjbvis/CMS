46056f2b4955dd3365cbb5ef2c50436cefa7877e
Justin Field <fieldju@gmail.com>
2012-02-24 01:14:12 -0800

    css progress update

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -12,12 +12,12 @@
 	</head>
 	<body>
 		<div id="login">
-				<form method="POST">
-				Username:<br />
-				<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" class="form" /><?php echo form_error('username'); ?><br /><br />
-				Password:<br />
-				<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" class="form" /><?php echo form_error('password'); ?><br /><br />
-				<input type="submit" value="Login" name="login" />
+				<form name="loginform" method="POST">
+					<label>Username: </label>
+						<input type="text" name="username" value="<?php echo set_value('username'); ?>" /><?php echo form_error('username'); ?><br /><br />
+					<label>Password:  </label>
+						<input type="password" name="password" value="<?php echo set_value('password'); ?>" /><?php echo form_error('password'); ?><br /><br />
+						<input type="submit" value="Login" name="login" />
 				</form>
 		</div>
 	</body>
