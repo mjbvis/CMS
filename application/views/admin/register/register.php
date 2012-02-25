8606c471f061d37c9a4f033d71e3f0cd993b5a77
Justin Field <fieldju@gmail.com>
2012-02-25 01:59:53 -0800

    created index.html files for new folders

--- application/views/admin/register/register.php
+++ application/views/admin/register/register.php
@@ -4,18 +4,17 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	</head>
 	<body>
-		
-		<h2>Register a new parent account</h2>
-		
-		
-		<div class="box">
-				<form method="post">
-					First Name: <input type="text" name="username" size="50" class="form" value="<?php echo set_value('first'); ?>" /><br /><?php echo form_error('first'); ?><br />
-					Last Name: <input type="text" name="username" size="50" class="form" value="<?php echo set_value('last'); ?>" /><br /><?php echo form_error('last'); ?><br />
-					
-					Email:<br />
-					<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email'); ?>" /><?php echo form_error('email'); ?><br /><br />
-					<input type="submit" value="Register" name="register" />
+		<div class="formBox">
+				<form method="post" id="registerUser">
+				    <fieldset>
+				    <legend><h2>Register a new parent account</h2></legend>
+    					First Name: <input type="text" name="username" size="50" class="form" value="<?php echo set_value('first'); ?>" /><br /><?php echo form_error('first'); ?><br />
+    					Last Name: <input type="text" name="username" size="50" class="form" value="<?php echo set_value('last'); ?>" /><br /><?php echo form_error('last'); ?><br />
+    					
+    					Email:<br />
+    					<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email'); ?>" /><?php echo form_error('email'); ?><br /><br />
+    					<input type="submit" value="Register" name="register" />
+					</fieldset>
 				</form>
 		</div>
 	</body>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,8 +6,8 @@
 		 	Email: <a href="mailto:info@corvallismontessori.org">info@corvallismontessori.org</a>
 		</div>
 	<div id="rss">
-		<a href="http://twitter.com/#!/cmsmont" target="_blank" title="Follow us on Twitter!"><img class="social" src="assets/images/tw_icon.png" border="0" /></a>
-		<a href="http://www.facebook.com/pages/Corvallis-Montessori-School/141276895968144" target="_blank" title="Click here to visit our Facebook page!"><img class="social" src="assets/images/fb_icon.png" border="0" /></a> 
+		<a href="http://twitter.com/#!/cmsmont" target="_blank" title="Follow us on Twitter!"><img class="social" src="<?php echo base_url('assets/images/tw_icon.png'); ?>" border="0" /></a>
+		<a href="http://www.facebook.com/pages/Corvallis-Montessori-School/141276895968144" target="_blank" title="Click here to visit our Facebook page!"><img class="social" src="<?php echo base_url('assets/images/fb_icon.png'); ?>" border="0" /></a> 
 	</div>
 </div>
 </body>
