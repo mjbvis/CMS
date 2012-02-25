12e145f96ce17035aa349bd35f2942cb0025321b
Justin Field <fieldju@gmail.com>
2012-02-25 01:12:57 -0800

    finished login screen, the lost your password link is just there for looks

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -11,13 +11,21 @@
 		<link rel="stylesheet" href="<?php echo base_url('assets/styles/login/screen.css'); ?>" type="text/css"	media="screen" />
 	</head>
 	<body>
-		<div id="login">
+		<div id="login" class="clearfix">
+				<img src="<?php echo base_url('assets/images/login_banner.png'); ?>" alt="CMS Logo" width="400" height="150">
 				<form name="loginform" method="POST">
 					<label>Username: </label>
 						<input type="text" name="username" value="<?php echo set_value('username'); ?>" /><?php echo form_error('username'); ?><br /><br />
 					<label>Password:  </label>
 						<input type="password" name="password" value="<?php echo set_value('password'); ?>" /><?php echo form_error('password'); ?><br /><br />
-						<input type="submit" value="Login" name="submit" class="submit"/>
+						
+						<div id="helpDiv">
+						    <a href="">Lost your password?</a>
+						</div>
+						
+						<div id="submitDiv">
+						  <input type="submit" value="Login" name="submit" class="submit"/>
+						</div>
 				</form>
 		</div>
 	</body>
