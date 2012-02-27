fde431f768a2c73190f1fe56ec872b93fd154ae7
GneatGeek <gneatgeek@gmail.com>
2012-02-26 19:48:57 -0800

    Fixed error caused if  is not passed to the template view.  Added isset_not_empty function

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,4 +1,14 @@
-<<<<<<< HEAD
+<?php
+/**
+ * Is Set Not Empty Function
+ * Use: Verify if a variable is set and is not empty.
+ * @param mixed $var - Reference of a variable to be checked.
+ * @return bool - True if the variable is set AND not empty.  False otherwise.
+ */
+function isset_not_empty(&$var){
+	return(isset($var) && !empty($var));
+}
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -7,59 +17,7 @@
 		<meta name="Description" content="Corvallis Montessori School">
 		<meta name="Author" content="Corvallis Montessori School">
 		<meta name="Rating" content="General">
-		<title><?php echo($title); ?> | CMS</title>
-		<link rel="icon" type="image/x-icon" href="<?php echo base_url('favicon.ico'); ?>" />
-		<link rel="stylesheet" href="<?php echo base_url('assets/styles/screen.css'); ?>" type="text/css"	media="screen" />
-	</head>
-	<body>
-		<div id="main">
-			<div id="header">
-				<header>
-					<div id="log">Login/Logout Stuff Here</div>
-					<img src="<?php echo base_url('assets/images/cms_logo.png'); ?>" alt="CMS Logo" width="585" height="67">
-					<nav id="topNav">
-						<ul>
-							<li>Item 1</li>
-							<li>Item 2</li>
-							<li>
-								<a href="#">Item 3</a>
-								<ul>
-									<li>Sub1</li>
-									<li>Sub2</li>
-									<li>Sub3</li>
-								</ul>
-							</li>
-							<li>Item 4</li>
-							<li>Item 5</li>
-							<li>Item 6</li>
-							<li>Item 7</li>
-						</ul>
-					</nav>
-				</header>
-			</div>
-			<div id="sideBar">
-				<ul style="list-style:none">
-					<li>Item 1</li>
-					<li>Item 2</li>
-					<li>Item 4</li>
-					<li>Item 5</li>
-					<li>Item 6</li>
-					<li>Item 7</li>
-				</ul>
-			</div>
-			<div id="content">
-				
-<!-- End Header Segment -->
-=======
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<meta name="Robots" content="noindex, nofollow, noarchive">
-		<meta name="Description" content="Corvallis Montessori School">
-		<meta name="Author" content="Corvallis Montessori School">
-		<meta name="Rating" content="General">
-		<title><?php echo($title); ?> | CMS</title>
+		<title><?php echo((isset_not_empty($title) ? "$title | CMS" : "CMS")); ?></title>
 		<link rel="icon" type="image/x-icon" href="<?php echo base_url('favicon.ico'); ?>" />
 		<link rel="stylesheet" href="<?php echo base_url('assets/styles/screen.css'); ?>" type="text/css"	media="screen" />
 		<link rel="stylesheet" href="<?php echo base_url('assets/styles/forms/screen.css'); ?>" type="text/css" media="screen" />
@@ -85,6 +43,4 @@
 				</ul>
 			</nav>
 			<div id="content">
-				
 <!-- End Header Segment -->
->>>>>>> f89257a2aba01cba4c82e8baae05ea367c4ca8e4
