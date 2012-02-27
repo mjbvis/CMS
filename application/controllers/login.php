7fcfd6e7726474e84fa1a9273fec5b8bf9be6936
kosw <kos.wesley@gmail.com>
2012-02-26 18:28:51 -0800

    fixed last of broken files

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-class Login extends Application {
-
-	function __construct() {
-		parent::__construct();
-
-		# Load Helpers
-		$this->load->helper(array('url', 'form', 'dashboard'));
-
-		# Load Libraries
-		
-		# Load Modules
-		$this->load->model('login/Login_model');
-	}
-
-	# This is the default login view
-	function index() {
-		$data          = array();
-		$data['title'] = 'Login';
-		
-
-		//if the user is already loged in goto their default dashboard
-		if(logged_in())
-		{
-			// get all alerts for current user
-			$alerts = $this->Login_model->selectUserAlerts(user_id());
-			// redirect to the appropriate dashboard
-			redirect(get_dashboard($alerts));
-		}
-		else // else present them with the login page.
-		{
-			$this->login();
-		}
-	}
-	
-}
-
-?>
-=======
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Login extends Application {
 
@@ -77,4 +37,3 @@ class Login extends Application {
 }
 
 ?>
->>>>>>> f89257a2aba01cba4c82e8baae05ea367c4ca8e4
--- application/helpers/ag_auth_helper.php
+++ application/helpers/ag_auth_helper.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 function logged_in()
@@ -49,58 +48,4 @@ function group_table()
 	
 	return $CI->ag_auth->group_table;
 }
-
-=======
-<?php
-
-function logged_in()
-{
-	$CI =& get_instance();
-	if($CI->ag_auth->logged_in() == TRUE)
-	{
-		return TRUE;
-	}
-	
-	return FALSE;
-}
-
-function username()
-{
-	$CI =& get_instance();
-	return $CI->session->userdata('username');
-}
-
-function user_id()
-{
-	$CI =& get_instance();
-	return $CI->session->userdata('id');
-}
-
-function user_group($group)
-{
-	$CI =& get_instance();
-	
-	$system_group = $CI->ag_auth->config['auth_groups'][$group];
-	
-	if($system_group === $CI->session->userdata('group_id'))
-	{
-		return TRUE;
-	}
-}
-
-function user_table()
-{
-	$CI =& get_instance();
-	
-	return $CI->ag_auth->user_table;
-}
-
-function group_table()
-{
-	$CI =& get_instance();
-	
-	return $CI->ag_auth->group_table;
-}
-
->>>>>>> f89257a2aba01cba4c82e8baae05ea367c4ca8e4
 ?>
