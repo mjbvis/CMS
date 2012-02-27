edee6bb3653d05e95e9ea0306f4a2be8c863a969
kosw <kos.wesley@gmail.com>
2012-02-26 20:14:01 -0800

    fixing more merge problems

--- application/controllers/alerts.php
+++ application/controllers/alerts.php
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-class Alerts extends Application
-{
-	public function __construct()
-	{
-		parent::__construct();
-		
-		/*restrict access to all but admin*/
-		//$this->ag_auth->restrict('alert');
-		
-		/* Load helpers */
-		$this->load->helper(array('url', 'form'));
-
-		/* Load libraries */
-		$this->load->library('form_validation');
-	}
-	
-	public function index()
-	{
-		if(logged_in())
-		{
-			$data = array();
-			$data['title'] = 'Alert Dashboard';
-	
-			/* load views */
-			$this->load->view('alerts/templates/header', $data);
-			$this->load->view('alerts/dashboard', $data);
-			$this->load->view('alerts/templates/footer', $data);
-		}
-		else
-		{
-			$this->login();
-		}
-	}
-
-}
-
-/* End of file: dashboard.php */
-=======
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Alerts extends Application
@@ -78,5 +37,4 @@ class Alerts extends Application
 }
 
 /* End of file: dashboard.php */
->>>>>>> f89257a2aba01cba4c82e8baae05ea367c4ca8e4
 /* Location: application/controllers/admin/dashboard.php */
