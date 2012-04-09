251338073973186583c7df54f21649c6ec2e4266
GneatGeek <gneatgeek@gmail.com>
2012-04-09 00:20:48 -0700

    blah

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -7,26 +7,25 @@
 		<meta name="Author" content="Corvallis Montessori School">
 		<meta name="Rating" content="General">
 		<title>login | CMS</title>
-		<link rel="icon" type="image/x-icon" href="<?php echo base_url('favicon.ico'); ?>" />
-		<link rel="stylesheet" href="<?php echo base_url('assets/styles/login/screen.css'); ?>" type="text/css"	media="screen" />
+		<link rel="icon" type="image/x-icon" href="<?php echo base_url('favicon.ico');?>" />
+		<link rel="stylesheet" href="<?php echo base_url('assets/styles/login/screen.css');?>" type="text/css"	media="screen" />
 	</head>
 	<body>
-		<div id="login" class="clearfix">
-				<img src="<?php echo base_url('assets/images/login_banner.png'); ?>" alt="CMS Logo" width="400" height="150">
-				<form name="loginform" method="POST">
-					<label>Username: </label>
-						<input type="text" name="username" value="<?php echo set_value('username'); ?>" /><?php echo form_error('username'); ?><br /><br />
-					<label>Password:  </label>
-						<input type="password" name="password" value="<?php echo set_value('password'); ?>" /><?php echo form_error('password'); ?><br /><br />
-						
-						<div id="helpDiv">
-						    <a href="">Lost your password?</a>
-						</div>
-						
-						<div id="submitDiv">
-						  <input type="submit" value="Login" name="submit" class="submit"/>
-						</div>
-				</form>
+		<div id="login">
+			<img src="<?php echo base_url('assets/images/login_banner.png');?>" alt="CMS Logo" width="400" height="150">
+			<form name="loginform" method="POST">
+				<label>Username: </label>
+				<input type="text" name="username" value="<?php echo set_value('username');?>" />
+				<?php echo form_error('username');?><br />
+				<br />
+				<label>Password: </label>
+				<input type="password" name="password" value="<?php echo set_value('password');?>" />
+				<?php echo form_error('password');?><br />
+				<div id="bottom">
+					<input type="submit" value="Login" name="submit" class="submit"/>
+					<a href="">Lost your password?</a>
+				</div>
+			</form>
 		</div>
 	</body>
 </html>
