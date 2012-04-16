54843a1dabf38cb32ad7b41f907da82afe93b5e4
markb <markbowser9@gmail.com>
2012-04-16 04:35:04 -0700

    Added content to the policy page

--- application/views/admissions/forms/policy.php
+++ application/views/admissions/forms/policy.php
@@ -1,1 +1,47 @@
-hello. school policy. here. later.
+<div class="formBox">
+	<form id="montessori_policy">
+		<fieldset>
+			<legend>
+				Corvallis Montessori School
+			</legend>
+	
+			<fieldset>
+				<legend>Respect:</legend>
+				<li>We respect the child by encouraging him to do as many things as possible for himself.</li></br>
+				<li>Grace and courtesy lessons (walking around another child's work area).</li></br>
+				<li>We expect children show respect by obeying adfieldsetts at school and at home.</li></br>
+				<li>We encourage parents to model appropriate behavior at home and in social situations.</li></br>
+				<li>We ask that the parent's aid in their child's growing independence.</li></br>
+				<li>Importance of mixed age groups.</li></br>
+			</fieldset>
+			<fieldset>
+				<legend>Responsibility:</legend>
+				<li>Responsibility for self may include zipping his/her own coat (sees other children doing it and wants to do it as well).</li></br>
+				<li>Zipping another child's coat.</li></br>
+				<li>Straightening class at the end of the day.</li></br>
+				<li>Be specific about what the class expectation is and how you are going to help the child.</li></br>
+				<li>At home <li>unloading groceries and putting them away.</li></br>
+				<li>Cleaning out the sink after liushing teeth.</li></br>
+				<li>Watering the plants or feeding a pet.</li></br>
+				<li>Student teacher ratio and mixed age group.</li></br>
+			</fieldset>
+			<fieldset>
+				<legend>Resourcefulness:</legend>
+				<li>Directing course of one's own day.</li></br>
+				<li>Mediation and problem solving in the classroom.</li></br>
+				<li>At home, encouraging parents to resist the impfieldsetse to solve all of their child's problems, allowing stuggle.</li></br>
+				<li>Encourage parents to participate in problem solving with their child.</li></br>
+				<li>Importance of high student-teacher ratio.</li></br>
+			</fieldset>
+			<fieldset>
+				<legend>Other:</legend>
+				<li>Routine of day (lessons, snack, gym, times that teachers canbe reached, partnership).</li></br>
+				<li>Toileting, workable clothing, carpools/pick-up.</li></br>
+				<li>Food allergies</li></br>
+				<li>Sick or vacation, call office</li></br>
+				<li>Parent education and Dreikurs</li></br>
+				<li>The use of TV and other media for young children.</li></br>
+			</fieldset>
+		</fieldset>
+	</form>
+</div>
