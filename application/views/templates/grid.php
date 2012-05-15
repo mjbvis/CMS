307d83a295e61482b4d1c9c61741024f1f19af74
markb <markbowser9@gmail.com>
2012-05-15 16:43:27 -0700

    added the grid to the templates

---
+++ application/views/templates/grid.php
@@ -0,0 +1,16 @@
+<?php 
+foreach($css_files as $file): ?>
+    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+ 
+<?php endforeach; ?>
+<?php foreach($js_files as $file): ?>
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+
+<div id="content">
+	<?php echo $output; ?>
+	
+</div>
+
+
+
