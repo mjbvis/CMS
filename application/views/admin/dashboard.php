7a525efa8c55cbd5a1f885a6a3aca5616ef03e7c
markb <markbowser9@gmail.com>
2012-05-15 19:27:01 -0700

    git is being crazy and it really wants me to commit this. It shouldn't actually change anything

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,9 +1,22 @@
-<div id="content">
-	
-	<IFRAME SRC=<?php echo base_url('admin/waitlistGrid');?> class="autoHeight" WIDTH=53% frameborder="0"></IFRAME>
-	</br>
-	<IFRAME SRC=<?php echo base_url('admin/preEnrolledGrid');?> class="autoHeight" WIDTH=53% frameborder="0"></IFRAME>
-	</br>
-	<IFRAME SRC=<?php echo base_url('admin/volunteerLogGrid');?> class="autoHeight" WIDTH=53% frameborder="0"></IFRAME>
+<style type="text/css">
+	table, iframe{ width: 100%;	}
+	table{
+		text-align:center;
+		border-collapse: collapse;
+	}
+</style>
 
-</div>
+<table border="1">
+	<tr>
+		<td valign="top" width="300">
+			<IFRAME SRC=<?php echo base_url('admin/waitlistGrid'); ?> class="autoHeight" frameborder="0"></IFRAME>
+		</td>
+		<td valign="top">
+			<IFRAME SRC=<?php echo base_url('admin/preEnrolledGrid'); ?> class="autoHeight" frameborder="0"></IFRAME>
+			<IFRAME SRC=<?php echo base_url('admin/volunteerLogGrid'); ?> class="autoHeight" frameborder="0"></IFRAME>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" valign="top"><IFRAME SRC=<?php echo base_url('admin/volunteerLogGrid'); ?> class="autoHeight" frameborder="0"></IFRAME></td>
+	</tr>
+</table>
