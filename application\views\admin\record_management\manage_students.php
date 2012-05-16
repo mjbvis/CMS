b9429d3c25ee3ad3360d7e9032950b745a0bb63c
Justin Field <fieldju@gmail.com>
2012-05-16 03:13:29 -0700

    trying to delete wiered \ files

--- application\views\admin\record_management\manage_students.php
+++
@@ -1,3 +0,0 @@
-<IFRAME SRC=<?php echo base_url('admin/studentGrid'); ?> class="autoHeight" width=100% frameborder="0"></IFRAME>
-<IFRAME SRC=<?php echo base_url('admin/studentEducBackgroundGrid'); ?> class="autoHeight" width=100% frameborder="0"></IFRAME>
-<IFRAME SRC=<?php echo base_url('admin/studentMedicalInformationGrid'); ?> class="autoHeight" width=100% frameborder="0"></IFRAME>
