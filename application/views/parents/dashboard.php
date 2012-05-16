cd4c48ac63a90172f63c848c75a98def230a696a
markb <markbowser9@gmail.com>
2012-05-15 20:20:32 -0700

    converted carbo grid to grocery_crud on the parent dashboard. Still working on the add volunteer log entry functionality.

--- application/views/parents/dashboard.php
+++ application/views/parents/dashboard.php
@@ -1,42 +1,19 @@
-<script src="http://code.jquery.com/jquery-1.7.min.js" type="text/javascript"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>js/jquery.bbq.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>js/jquery.form.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>js/jquery.timepicker.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>js/carbo.grid.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>js/carbo.form.js" type="text/javascript"></script>
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/carbo/jquery-ui-1.8.16.custom.css" type="text/css" media="all" />
-<link href="<?php echo base_url(); ?>css/carbo.grid.css" rel="stylesheet" type="text/css" media="all" />
-<!--[if lt IE 7]><link href="<?php echo base_url(); ?>css/carbo.grid.ie6.css" rel="stylesheet" type="text/css" media="all" /><![endif]-->
-<link href="<?php echo base_url(); ?>css/carbo.form.css" rel="stylesheet" type="text/css" media="all" />
-<link href="<?php echo base_url(); ?>css/960.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet" type="text/css" />
+<div id="waitlistForm">
+	<IFRAME SRC=<?php echo base_url('parents/waitlistGrid');?> WIDTH=40% class="autoHeight" frameborder="0"></IFRAME>
+</div>
 
-<div id="content">
+<div id="preEnrolledForm">
+	<IFRAME SRC=<?php echo base_url('parents/preEnrolledGrid');?> WIDTH=40% class="autoHeight" frameborder="0"></IFRAME>
+</div>
 
-	<div id="waitlistForm">
-		<label>Waitlisted:</label></br>
-		<?php if (isset($wlGrid)) echo $wlGrid; ?>
-	</div>
-	
-	<div id="preEnrolledForm">
-		<label>Pre-Enrolled:</label></br>
-		<?php if (isset($preEnrollGrid)) echo $preEnrollGrid; ?>
-	</div>
-	
-	<div id="registeredForm">
-		<label>Registered:</label></br>
-		<?php if (isset($registeredGrid)) echo $registeredGrid; ?>
-	</div>
-	
-	<div id="NotificationForm">
-		<label>Registered:</label></br>
-		<?php if (isset($NotificationGrid)) echo $NotificationGrid; ?>
-	</div>
-	
-	<div id="VolunteeringForm">
-		<label>Volunteer Log:</label></br>
-		<?php if (isset($volunteeringGrid)) echo $volunteeringGrid; ?>
-	</div>
+<div id="registeredForm">
+	<IFRAME SRC=<?php echo base_url('parents/registeredGrid');?> WIDTH=40% class="autoHeight" frameborder="0"></IFRAME>
+</div>
 
+<!-- <div id="NotificationForm">
+	<IFRAME SRC=<?php echo base_url('parents/notificationGrid');?> WIDTH=40% class="autoHeight" frameborder="0"></IFRAME>
+</div> -->
+
+<div id="VolunteeringForm">
+	<IFRAME SRC=<?php echo base_url('parents/volunteeringGrid');?> WIDTH=100% class="autoHeight" frameborder="0"></IFRAME>
 </div>
