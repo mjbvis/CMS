2151429cb2bdf2268bcd816f14e388ebecc9ff4f
GneatGeek <gneatgeek@gmail.com>
2012-05-31 23:54:02 -0700

    more updates

--- application/views/admissions/admin_approval.php
+++ application/views/admissions/admin_approval.php
@@ -1,1 +1,5 @@
-Once you have completed step 4, you will wait for an admin to approve your application and place the child in a classroom.
+<div class="content_text_wrapper">
+	Once you have completed step 4, you will wait for an admin to approve your application and
+	place the child in a classroom.
+	<img src="../assets/images/filler2.png" width="722" height="230" style="margin:20px 0;">
+</div>
